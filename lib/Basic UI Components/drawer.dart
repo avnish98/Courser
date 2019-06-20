e8d635ec83f1d771e086d457fa221b9aae8f8659
avnish98 <av_nish@outlook.com>
2019-06-20 22:39:50 +0530

    Added missing folders

---
+++ lib/Basic UI Components/drawer.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+// From Courser
+import 'package:courser/Pages/home_page.dart';
+import "package:courser/Pages/add_courses.dart";
+import 'package:courser/Pages/added_courses.dart';
+import 'package:courser/Pages/upvoted_courses.dart';
+
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          new Container(
+            child: new DrawerHeader(
+              child: Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    'Username goes here',
+                    style: TextStyle(color: Colors.white),
+                  )),
+            ),
+            color: Colors.deepPurple,
+          ),
+          new ListTile(
+            title: new Text('Home '),
+            onTap: () {
+              Navigator.push(context,
+                  new MaterialPageRoute(builder: (BuildContext) => callHome()));
+            },
+          ),
+          new ListTile(
+            title: new Text('Add a new course'),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (BuildContext) => AddCourses()));
+            },
+          ),
+          new ListTile(
+            title: new Text('Added courses'),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (BuildContext) => AddedCourses()));
+            },
+          ),
+          new ListTile(
+            title: new Text('Upvoted courses'),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                      builder: (BuildContext) => UpvCourses()));
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Course/course_desc.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+
+// From Courser
+import 'package:courser/Basic UI Components/basicUI.dart';
+import 'package:courser/DB Interface/structures.dart';
+
+class CourseDesc extends StatelessWidget {
+  Course currCourse;
+
+  CourseDesc(Course c1) {
+    this.currCourse = c1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // App bar of Course page
+    final courseDescBar = AppBar(
+        title: Text(
+          "Course Details",
+          style: TextStyle(color: Colors.deepPurple),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.deepPurple,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ));
+
+    // TODO: Return a picture according to type of course
+    final courseImage = 0;
+
+    final linkButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(10.0),
+      color: Colors.deepPurple,
+      child: MaterialButton(
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text("LINK",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 21.0, color: Colors.white)),
+      ),
+    );
+
+    final likeButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(10.0),
+      color: Colors.white,
+      child: MaterialButton(
+          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          onPressed: () {},
+          child: Icon(
+            Icons.thumb_up,
+            color: Colors.deepPurple,
+          )),
+    );
+
+    final linkAndUpvote = Row(
+      children: <Widget>[
+        likeButton,
+        SizedBox(
+          width: 20.0,
+        ),
+        titleGen("${currCourse.upvCount}", 18.0, FontWeight.bold, Colors.black),
+        SizedBox(
+          width: 100.0,
+        ),
+        linkButton
+      ],
+    );
+
+    // Reviews
+    final reviewBox = TextField(
+        decoration: InputDecoration(hintText: "Add your comment here....."));
+
+    final reviewSubButton =
+        ButtonGen(context, "SUBMIT REVIEW", Colors.white, Colors.black);
+
+    final reviewContainer = new SizedBox(
+        height: 500.0,
+        child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (BuildContext context, index) {
+              return Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: Card(
+                    elevation: 5.0,
+                    child: Column(
+                      children: <Widget>[
+                        titleGen("Username", 12.0, FontWeight.bold,
+                            Colors.deepPurple),
+                        Text(
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ")
+                      ],
+                    ),
+                  ));
+            }));
+
+    final spacerCourseDesc = SizedBox(
+      height: 20.0,
+    );
+
+    return Scaffold(
+        appBar: courseDescBar,
+        body: SingleChildScrollView(
+            child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              titleGen(currCourse.cname, 32.0, FontWeight.bold, Colors.black),
+              spacerCourseDesc,
+              titleGen("Description", 18.0, FontWeight.bold, Colors.black),
+              valueGen('${currCourse.desc}'),
+              spacerCourseDesc,
+              titleGen("Platform", 18.0, FontWeight.bold, Colors.black),
+              valueGen(currCourse.platform),
+              spacerCourseDesc,
+              titleGen("Pre-Requisites", 18.0, FontWeight.bold, Colors.black),
+              valueGen(currCourse.preReq),
+              spacerCourseDesc,
+              titleGen("Type", 18.0, FontWeight.bold, Colors.black),
+              valueGen(currCourse.type),
+              spacerCourseDesc,
+              titleGen("Price", 18.0, FontWeight.bold, Colors.black),
+              valueGen(iffree(currCourse.free)),
+              spacerCourseDesc,
+              titleGen("Added by", 18.0, FontWeight.bold, Colors.black),
+              valueGen(currCourse.uname),
+              spacerCourseDesc,
+              linkAndUpvote,
+              spacerCourseDesc,
+              titleGen("Reviews", 18.0, FontWeight.bold, Colors.black),
+              reviewBox,
+              spacerCourseDesc,
+              reviewSubButton,
+              spacerCourseDesc,
+              titleGen("Reviews", 18.0, FontWeight.bold, Colors.black),
+              reviewContainer
+            ],
+          ),
+        )));
+  }
+}
