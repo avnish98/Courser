719636805ce203b45f3a728474b1a3feb45a09af
avnish98 <av_nish@outlook.com>
2019-06-20 19:41:27 +0530

    Removed unnecessary files

--- lib/for_course_desc.dart
+++
@@ -1,8 +0,0 @@
- RaisedButton( padding: const EdgeInsets.all(8.0),
-                    textColor: Colors.white,
-                    color: Colors.blue,
-                    onPressed: (){ Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => SecondPage()),
-  );},
-                    child: new Text("Route"),),
