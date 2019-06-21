637e5305bbc854ee597d44f99d5279f6e9579893
avnish98 <av_nish@outlook.com>
2019-06-21 14:49:50 +0530

    Updated course database

--- lib/Pages/add_courses.dart
+++ lib/Pages/add_courses.dart
@@ -49,7 +49,8 @@ class _AddCourseState extends State<AddCourses> {
             return "Please enter a valid Course name";
           }
         },
-        decoration: InputDecoration(hintText: "JavaScript for Web Development"));
+        decoration:
+            InputDecoration(hintText: "JavaScript for Web Development"));
 
     //Course Platform
     final platformText =
@@ -88,7 +89,8 @@ class _AddCourseState extends State<AddCourses> {
             return "Please enter a valid Course name";
           }
         },
-        decoration: InputDecoration(hintText: "https://www.example.com/"));;
+        decoration: InputDecoration(hintText: "https://www.example.com/"));
+    ;
 
     // Type of Course
     final typeText = titleGen("Type", 12.0, FontWeight.bold, Colors.grey);
@@ -153,16 +155,49 @@ class _AddCourseState extends State<AddCourses> {
             return "Please enter a valid description";
           }
         },
-        decoration: InputDecoration(hintText: "JavaScript is a client side language"));
+        decoration:
+            InputDecoration(hintText: "JavaScript is a client side language"));
 
     // Padding between textfields
     final spacer = SizedBox(height: 10.0);
 
-    // Submit button
-    final sButton = FloatingActionButton(backgroundColor:Colors.black, onPressed: (){Course(11, cnameController.text, "Developer", descController.text, this._courseTypeSelected,
-        linkController.text, platformController.text, 0, _priceTypeSelected, prereqController.text).ConsoleDisplay();},);
-        //ButtonGen(context, 'SUBMIT', Colors.white, Colors.deepPurple);
+    Map payLoad = {
+      "cid": 11,
+      "cname": cnameController.text,
+      "uname": 'Devloper',
+      "desc": descController.text,
+      "type": this._courseTypeSelected,
+      "link": linkController.text,
+      "platform": platformController.text,
+      "upvcount": 0,
+      "price": this._priceTypeSelected,
+      "preReq": prereqController.text
+    };
 
+    void createCourse(Map courseMap) {
+      Course c1;
+      c1.loadData(courseMap);
+      c1.ConsoleDisplay();
+    }
+
+    // Submit button
+    final sButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(10.0),
+      color: Colors.deepPurple,
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {
+          createCourse(payLoad);
+        },
+        child: Text("Add Course",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 21.0, color: Colors.white)),
+      ),
+    );
+
+    //ButtonGen(context, 'SUBMIT', Colors.white, Colors.deepPurple);
     return Scaffold(
         key: _scaffoldKey,
         appBar: topBar,
