dc9d1b061aa967de0328c254f578971ec1eb23b5
niteshnanda02 <niteshnanda02@gmail.com>
2019-06-24 05:44:54 +0530

    add some cources widget

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -50,7 +50,52 @@ Widget callHome() {
       12,
       'Free',
       "JS");
-  List<Course> courseList = [currCourse, currCourse2, currCourse3, currCourse4];
+Course currCourse5 = Course(
+      11,
+      "Advanced Python Programming 4 ",
+      'Developer',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Python',
+      'https://www.example.com',
+      'Udacity',
+      12,
+      'Free',
+      "JS");
+Course currCourse6 = Course(
+      11,
+      "Advanced Python Programming 4 ",
+      'Developer',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Python',
+      'https://www.example.com',
+      'Udacity',
+      12,
+      'Free',
+      "JS");
+Course currCourse7 = Course(
+      11,
+      "Advanced Python Programming 4 ",
+      'Developer',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Python',
+      'https://www.example.com',
+      'Udacity',
+      12,
+      'Free',
+      "JS");
+Course currCourse8 = Course(
+      11,
+      "Advanced Python Programming 4 ",
+      'Developer',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Python',
+      'https://www.example.com',
+      'Udacity',
+      12,
+      'Free',
+      "JS");
+
+  List<Course> courseList = [currCourse, currCourse2, currCourse3, currCourse4,currCourse5,currCourse6,currCourse7,currCourse8];
 
   return (MyHomePage(courseList));
 }
